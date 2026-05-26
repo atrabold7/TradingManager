3e95a6174a4cc6497a12ccd5cb8420240621b067
Andreas Trabold <a.trabold@outlook.com>
2026-05-26 22:52:44 +0200

    include new method

--- Trade.h
+++ Trade.h
@@ -40,5 +40,5 @@ public:
     std::optional<std::chrono::year_month_day> getSellDate() const;
     int getHoldingPeriod() const;
     bool getTradeClosed() const;
-
+    void printTrade() const;
     };
