b0cd2c4d1e159cc44d19416c46dcca6e73c5bc9c
Andreas Trabold <a.trabold@outlook.com>
2026-05-30 21:59:16 +0200

    initial commit

---
+++ FileTradeRepository.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "ITradeRepository.h"
+
+class FileTradeRepository : public ITradeRepository
+{
+public:
+    
+    virtual void saveData(const std::vector<Trade>& trades) override;
+    virtual void readTrades(std::vector<Trade>& trades) override;
+};
