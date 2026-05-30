59a1122c7b0b05a2444dec7fbf78e6ae28b18579
Andreas Trabold <a.trabold@outlook.com>
2026-05-30 18:25:55 +0200

    initial commit

---
+++ TradePrinter.cpp
@@ -0,0 +1,39 @@
+﻿#include "TradePrinter.h"
+#include "Trade.h"
+#include "Portfolio.h"
+#include <iostream>
+#include <iomanip>
+
+void TradePrinter::printTrade(int StockId, const Trade &trade)
+    {
+    std::cout << std::left << std::setw(5) << "[" << StockId << "]"
+        << std::left << std::setw(15) << trade.getStockName()
+        << std::setw(10) << trade.getStockAmount()
+        << std::setw(15) << trade.getSingleBuyPrice()
+        << std::setw(15) << trade.getBuyDate()
+        << std::setw(15) << trade.getSingleSellPrice()
+        //<< " | Menge: " << std::setw(15) << m_SellDate
+        << std::setw(15) << trade.getHoldingPeriod()
+        << std::setw(15) << (trade.getTradeClosed() ? "Closed" : "Open")
+        << std::endl;
+    }
+
+void TradePrinter::printAll(const Portfolio& m_portfolio) {
+    int StockId = 0;
+    
+    std::cout << std::left << std::setw(5) << "Id"
+        << std::left << std::setw(15) << "Stock Name"
+        << std::setw(10) << "Qty"
+        << std::setw(15) << "Buy price"
+        << std::setw(15) << "Buy date"
+        << std::setw(15) << "Sell price"
+        //<< " | Menge: " << std::setw(15) << m_SellDate
+        << std::setw(15) << "Hold time"
+        << std::setw(15) << "Status"
+        << std::endl << std::endl;
+
+    for (const auto& trade : m_portfolio.getTrades()) {
+        std::cout << "==============================================================================================" << std::endl;
+        TradePrinter::printTrade(++StockId, trade);
+        }
+    }
---
+++ TradePrinter.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+class Portfolio;
+class Trade;
+
+class TradePrinter
+{
+public:
+	static void printTrade(int StockId, const Trade &trade) ;
+	static void printAll(const Portfolio& m_portfolio);
+};
