49ec2b23d7916643d16cec3d441eda64ad6d49cf
Andreas Trabold <a.trabold@outlook.com>
2026-06-03 20:56:44 +0200

    delete useless files

--- FileTradeRepository.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include "ITradeRepository.h"
-
-class FileTradeRepository : public ITradeRepository
-{
-public:
-    virtual void saveData(const std::vector<Trade>& trades) override;
-    virtual void readTrades(std::vector<Trade>& trades) override;
-};
