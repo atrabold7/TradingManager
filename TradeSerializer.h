5d3a28cc9f23799c9a667f4b0c6c6131d516125b
Andreas Trabold <a.trabold@outlook.com>
2026-06-03 11:30:03 +0200

    initial commit

---
+++ TradeSerializer.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <nlohmann/json.hpp>
+#include "Trade.h"
+
+using json = nlohmann::json;
+
+namespace TradeSerializer
+{
+    void to_json(json& j, const Trade& t);
+    void from_json(const json& j, Trade& t);
+    void readTrades(std::ifstream& file, std::vector<Trade>& trades);
+    void saveData(std::ofstream& file, const std::vector<Trade>& trades);
+}
