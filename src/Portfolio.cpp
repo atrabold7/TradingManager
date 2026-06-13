278cd71da79d44c5f030d02728e3133f72e8fbc9
Andreas Trabold <a.trabold@outlook.com>
2026-06-13 23:03:33 +0200

    calculate win fixed

--- src/Portfolio.cpp
+++ src/Portfolio.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 #include "TradeInputHandler.h"
 
-float Portfolio::calculateTotalWin() {
-    float totalWin = 0;
+float Portfolio::calculateTotalNetWin() {
+    float totalNetWin = 0;
 
     for (const Trade& trade : m_trades) {
         if (trade.getTradeClosed()) {
@@ -13,19 +13,50 @@ float Portfolio::calculateTotalWin() {
                 + trade.getTax()
                 + trade.getBuyFee();
 
-            totalWin += revenue - expenses;
+            totalNetWin += revenue - expenses;
             }
         }
-
-    return totalWin;
+    return totalNetWin;
     }
+float Portfolio::calculateTotalGrossWin() {
+    float totalGrossWin = 0;
+
+    for (const Trade& trade : m_trades) {
+        if (trade.getTradeClosed()) {
+            float revenue = trade.getStockAmount() * trade.getSingleSellPrice();
+            float expenses = trade.getStockAmount() * trade.getSingleBuyPrice();
+
+            totalGrossWin += revenue - expenses;
+        }
+    }
+    return totalGrossWin;
+}
+float Portfolio::calculateTotalFee() {
+    float totalFee = 0;
+
+    for (const Trade& trade : m_trades) {
+        if (trade.getTradeClosed()) {
+            totalFee += trade.getBuyFee() + trade.getSellFee();
+        }
+    }
+    return totalFee;
+}
+float Portfolio::calculateTotalTax() {
+    float totalTax = 0;
+
+    for (const Trade& trade : m_trades) {
+        if (trade.getTradeClosed()) {
+            totalTax += trade.getTax();
+        }
+    }
+    return totalTax;
+}
 void Portfolio::addTrade(TradeInputData tradeInputData)
 {
     m_trades.emplace_back(
         tradeInputData.m_StockName,
         tradeInputData.m_StockAmount,
         tradeInputData.m_SingleBuyPrice,
-        tradeInputData.m_Tax,
         tradeInputData.m_BuyFee,
         tradeInputData.m_BuyDate);
 }
@@ -44,7 +75,7 @@ void Portfolio::changeTrade(Trade &trade, TradeData tradeData)
     
     if (tradeData.sellDate.has_value())
         trade.setSellDate(tradeData.sellDate.value());
-        
-    trade.setHoldingPeriod(tradeData.holdingPeriod);
+    
+    trade.setTax(tradeData.tax);
     trade.setTradeClosed(true);
 }
