bad3f6114ba8f20a7d366291b74658e416aedb81
Andreas Trabold <a.trabold@outlook.com>
2026-06-15 10:38:28 +0200

    new constants

--- src/TradingConstants.h
+++ src/TradingConstants.h
@@ -1,7 +1,10 @@
 ﻿#pragma once
+#include <string_view>
 
 namespace Trading {
-    static constexpr long long SCALE = 1000;
+    static constexpr long long SCALE = 100000000;
+    static constexpr std::string_view FILESTOCKS {"portfolio_stock.json"};
+    static constexpr std::string_view FILESCRYPTO {"portfolio_crypto.json"};
     
     // helper method
     inline double longlongToDisplay(long long value)
